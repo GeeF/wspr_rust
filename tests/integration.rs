522ece4c3aab330c9a906c8ad8724c5b7a6a763a
GeeF <andre@luebken.org>
2020-07-19 18:57:57 +0200

    Integration test and python generator script to generate a few thousand conformant messages
    The script will use the WSPRCode utility of the reference implementation to generate the correct channel symbols that are used during the test to compare the wspr_rust output against

--- tests/integration.rs
+++ tests/integration.rs
@@ -1,11 +1,39 @@
 use wspr::WSPRMessage;
+use std::fs::File;
+use std::path::{Path, PathBuf};
+use std::io::{self, BufRead};
 
+/// test generated test messages from wspr.txt that contain the channel encoded
+/// symbols encoded by the reference implementation
 #[test]
 fn integration() {
-    let msg = WSPRMessage::new("DB2LA", "JO43", 30);
-    let channel_encoded = msg.encode().unwrap();
-    for i in channel_encoded.iter() {
-        print!("{}, ", i);
+    let mut testdata_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    testdata_path.push("tests");
+    testdata_path.push("wspr.txt");
+
+    if let Ok(lines) = read_lines(testdata_path) {
+        let lines_iter = lines.map(|l| l.unwrap());
+        for line in lines_iter {
+            let msg: Vec<&str> = line.trim().split(" ").collect();
+            let mut chan_symbols: [u8; 162] = [0; 162];
+            for (i, &sym) in msg.iter().skip(3).enumerate() {
+                chan_symbols[i] = sym.to_string().parse::<u8>().unwrap();
+            }
+            let wspr_msg = WSPRMessage::new(msg[0], msg[1], msg[2].to_string().parse::<u8>().unwrap());
+            println!("{} {} {}", wspr_msg.callsign, wspr_msg.locator, wspr_msg.power);
+            let channel_encoded = wspr_msg.encode().unwrap();
+
+            for (sym, reference) in channel_encoded.iter().zip(chan_symbols.iter()) {
+                //println!("{} {}", sym, reference);
+                
+                assert_eq!(sym, reference);
+            }
+        }
     }
-    println!();
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
 }
---
+++ tests/wspr_msg_generator.py
@@ -0,0 +1,85 @@
+# This script is used to generate a large number of WSPR messages
+# conforming to the WSPR spec. Callsigns are not necessarily ITU compliant
+# The encoded channel symbol sequence from the reference implementation is 
+# added so the wspr_rust integration test can compare its results
+
+import random
+import os
+import subprocess
+
+CHARS_ALPHA = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+CHARS_LOC = 'ABCDEFGHIJKLMNOPQR'
+CHARS_NUM = '1234567890'
+CHARS_ALPHANUM = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+CHARS_ALPHANUM_WITH_SPACE = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ 1234567890'
+POWERS = [0, 3, 7, 10, 13, 17, 20, 23, 27, 30, 33, 37, 40, 43, 47, 50, 53, 57, 60]
+
+def gen_callsign():
+    trailing_len = random.randint(0, 3)
+
+    cs = []
+    cs.append(random.choice(CHARS_ALPHANUM_WITH_SPACE))
+    cs.append(random.choice(CHARS_ALPHA))
+    cs.append(random.choice(CHARS_NUM))
+
+    if cs[0] == ' ' and trailing_len == 0: # if first char is space, at least a third is needed
+        trailing_len = 1
+
+    for i in range(trailing_len):
+        cs.append(random.choice(CHARS_ALPHA))
+    return ''.join(cs)
+
+def gen_locator():
+    loc = []
+    loc.append(random.choice(CHARS_LOC))
+    loc.append(random.choice(CHARS_LOC))
+    loc.append(random.choice(CHARS_NUM))
+    loc.append(random.choice(CHARS_NUM))
+    return ''.join(loc)
+
+if __name__ == '__main__':
+    try:
+        os.remove("wspr.txt")
+    except FileNotFoundError:
+        pass
+
+    for i in range(5000):
+        callsign = gen_callsign()
+        locator = gen_locator()
+        power = random.choice(POWERS)
+        print(callsign, locator, power)
+        try:
+            # somehow the wsprcode utility does not seem to encode correctly using subprocess
+            # pipe everything to file
+            os.system('WSPRcode "{} {} {}" >> wspr.txt'.format(callsign.strip(), locator, str(power)))
+        except:
+            print("Make sure WSPRCode.exe is in this path")
+            raise
+    
+    print('-----------------------')
+    msg_chan_sym = []
+    with open('wspr.txt') as f:
+        for line in f:
+            if 'Message:' in line:
+                msg = line[9:].strip()
+                while not 'Channel symbols' in line:
+                    line = next(f)
+                chan_sym = ''
+                for i in range(6):
+                    chan_sym += next(f).strip() + ' '
+                while not 'Decoded message:' in line:
+                    line = next(f)
+                decoded = line.split(' ')[2:5]
+                #
+                if not ' '.join(decoded) == msg:
+                    print("!!reference encoder mismatch, skipping this msg")
+                    print('  reference decoded: ' + ' '.join(decoded) + '  source: ' + msg)
+                    continue
+                msg_chan_sym.append(msg + ' ' + chan_sym)
+    
+    with open('wspr.txt', 'w') as f:
+        for m in msg_chan_sym:
+            f.write(m + '\n')
+
+
+    
